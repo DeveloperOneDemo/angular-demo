ac4be8f542b0dd1f33029524033037a84758ed1d
DeveloperOneDemo <developer2021one@gmail.com>
2022-03-02 15:47:32 -0500

    Route guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,16 +6,17 @@ import { CopyComponent } from './copy/copy.component';
 import { CounterComponent } from './counter/counter.component';
 import { DisplayComponent } from './display/display.component';
 import { ListFruitComponent } from './list-fruit/list-fruit.component';
+import { AuthGuard } from './user/auth.guard';
 import { LoginComponent } from './user/login/login.component';
 import { LogoutComponent } from './user/logout/logout.component';
 
 const routes: Routes = [
-  { path: "display", component: DisplayComponent },
-  { path: "copy", component: CopyComponent },
-  { path: "counter", component: CounterComponent },
-  { path: "list-fruit", component: ListFruitComponent },
-  { path: "book-crud", component: ListBookComponent },
-  { path: "book-edit/:myId", component: EditBookComponent },
+  { path: "display", component: DisplayComponent, canActivate: [AuthGuard]},
+  { path: "copy", component: CopyComponent, canActivate: [AuthGuard] },
+  { path: "counter", component: CounterComponent, canActivate: [AuthGuard] },
+  { path: "list-fruit", component: ListFruitComponent, canActivate: [AuthGuard] },
+  { path: "book-crud", component: ListBookComponent, canActivate: [AuthGuard] },
+  { path: "book-edit/:myId", component: EditBookComponent, canActivate: [AuthGuard] },
   { path: "login", component: LoginComponent },
   { path: "logout", component: LogoutComponent }
 ];
